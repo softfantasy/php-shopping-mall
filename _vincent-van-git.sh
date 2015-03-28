095d2f82100654274b37ba9ab10fbad0b2c9350b
Bobbie <softfantasy101@gmail.com>
2015-03-28 04:14:49 -0800

    last update

---
+++ _vincent-van-git.sh
@@ -0,0 +1,38 @@
+mkdir .vincents-canvas
+cd .vincents-canvas
+git init
+git commit --allow-empty --date "2015-02-09T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-09T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-11T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-14T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-14T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-16T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-17T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-17T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-19T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-19T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-21T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-21T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-24T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-25T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-25T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-02-27T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-02T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-02T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-07T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-09T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-09T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-11T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-11T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-12T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-12T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-17T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-17T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-18T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-18T04:14:49.773-08:00)}" -m "feature update"
+git commit --allow-empty --date "2015-03-28T04:14:49.773-08:00)}" -m "feature update"
+read -p "Press any key to resume ..."
+git add .
+git commit --date "2015-03-28T04:14:49.773-08:00)}" -m "last update"
+git remote add origin https://github.com/softfantasy/php-shopping-mall.git
+git push -u origin master
